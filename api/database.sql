f3c31b587a6e75287d732730e37585191aa0b390
louisfontaine <louis.fontaine@efrei.net>
2019-12-18 16:38:55 +0100

    database

---
+++ api/database.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS managerOneDatabase;
+
+CREATE DATABASE managerOneDatabase;
+
+USE managerOneDatabase;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE  `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(32) NOT NULL,
+  `email` varchar(32) NOT NULL
+);
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(300) NOT NULL,
+  `creation_date` date NOT NULL,
+  `status` ENUM ('TODO', 'DONE') NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO users (id, name, email) VALUES (null, 'louisfontaine',  'louis.fontaine@efrei.net');
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO tasks VALUES (null, 1, 'Talk to mama', 'talk to mama about how to wash my clothes', '2019-04-02 16:32:22', 'TODO');
---
+++ api/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "managerOne";
+	$conn = mysqli_connect($server, $username, $password, $db);
+?>
