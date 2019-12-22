d2d783a6062282471769c0838ed1777bb35e5c35
louisfontaine <louis.fontaine@efrei.net>
2019-12-22 20:41:28 +0100

    update of the design

--- front/userTasks.js
+++ front/userTasks.js
@@ -7,9 +7,9 @@ function loadTasks() {
         if (this.readyState == 4 && this.status == 200) {
             var tasksJson = JSON.parse(this.responseText);
             var taksHtml = "";
-            taksHtml += "<table> <thead> <tr> <td>ID</td> <td>User ID</td> <td>Title</td> <td>Email</td> <td>Description</td> <td>Creation Date</td> <td>Status</td> </tr> </thead> <tbody>"
+            taksHtml += "<table id='users'> <thead> <th>ID</th> <th>User ID</th> <th>Title</th> <th>Email</th> <th>Description</th> <th>Creation Date</th> <th>Status</th></thead> <tbody>"
             for (var i = 0; i < tasksJson.length; i++) {
-                taksHtml += "<tr><td>" + tasksJson[i].id + "</td><td>" + tasksJson[i].user_id + "</td><td>" + tasksJson[i].title + "</td><td>" + tasksJson[i].description + "</td><td>" + tasksJson[i].creation_date + "</td><td>" + tasksJson[i].status + "</td> <td><button id=\"" + tasksJson[i].id + "\" type=\"button\" onClick=\"deleteClick(this.id)\"> Delete </button></td></tr>"
+                taksHtml += "<tr><td>" + tasksJson[i].id + "</td><td>" + tasksJson[i].user_id + "</td><td>" + tasksJson[i].title + "</td><td>" + tasksJson[i].description + "</td><td>" + tasksJson[i].creation_date + "</td><td>" + tasksJson[i].status + "</td> <td><button class='button' id=\"" + tasksJson[i].id + "\" type=\"button\" onClick=\"deleteClick(this.id)\"> Delete </button></td></tr>"
             }
             taksHtml += "</tbody> </table>"
 
@@ -27,19 +27,17 @@ function loadTasks() {
 }
 
 // Used to delete a task
-function deleteClick(taskId)
-{
+function deleteClick(taskId) {
     var xhttp = new XMLHttpRequest();
     xhttp.open("DELETE", "http://localhost/manager.one/apiPhp/tasks/" + taskId, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {loadTasks();}
+        if (this.readyState == 4 && this.status == 200) { loadTasks(); }
     }
 }
 
 // Used to create a user
-function createTask()
-{
+function createTask() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost/manager.one/apiPhp/tasks", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
@@ -51,8 +49,8 @@ function createTask()
         $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
     }
 
-    xhttp.send(JSON.stringify({user_id: $_GET['user_id'], title: document.forms["createTaskForm"]["title"].value, description:document.forms["createTaskForm"]["description"].value, creation_date:document.forms["createTaskForm"]["creation_date"].value, status:document.forms["createTaskForm"]["status"].value}));
+    xhttp.send(JSON.stringify({ user_id: $_GET['user_id'], title: document.forms["createTaskForm"]["title"].value, description: document.forms["createTaskForm"]["description"].value, creation_date: document.forms["createTaskForm"]["creation_date"].value, status: document.forms["createTaskForm"]["status"].value }));
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {loadTasks();}
+        if (this.readyState == 4 && this.status == 200) { loadTasks(); }
     }
 }
--- front/users.js
+++ front/users.js
@@ -7,9 +7,9 @@ function loadUsers() {
         if (this.readyState == 4 && this.status == 200) {
             var usersJson = JSON.parse(this.responseText);
             var usersHtml = "";
-            usersHtml += "<table> <thead> <tr> <td>ID</td> <td>Name</td> <td>Email</td> </tr> </thead> <tbody>"
-            for (var i=0; i<usersJson.length; i++) {
-                usersHtml += "<tr><td>" + usersJson[i].id + "</td><td>" + usersJson[i].name + "</td><td>" + usersJson[i].email + "</td> <td><button id=\"" + usersJson[i].id + "\" type=\"button\" onClick=\"goToTask(this.id)\"> Tasks </button> <button id=\"" + usersJson[i].id + "\" type=\"button\" onClick=\"deleteClick(this.id)\"> Delete </button></td></tr>"
+            usersHtml += newFunction()
+            for (var i = 0; i < usersJson.length; i++) {
+                usersHtml += "<tr><td>" + usersJson[i].id + "</td><td>" + usersJson[i].name + "</td><td>" + usersJson[i].email + "</td> <td><button class='button' id=\"" + usersJson[i].id + "\" type=\"button\" onClick=\"goToTask(this.id)\"> Tasks </button> <button class='button' id=\"" + usersJson[i].id + "\" type=\"button\" onClick=\"deleteClick(this.id)\"> Delete </button></td></tr>"
             }
             usersHtml += "</tbody> </table>"
 
@@ -18,34 +18,36 @@ function loadUsers() {
     };
     xhttp.open("GET", "http://localhost/manager.one/apiPhp/users", true);
     xhttp.send();
+
+    function newFunction() {
+        return "<table id='users'> <thead> <th> ID </th> <th> Name </th> <th> Email </th> <th></th> </thead> <tbody>";
+    }
 }
 
 // Used to delete a user
-function deleteClick(UserId)
-{
+function deleteClick(UserId) {
     console.log("http://localhost/manager.one/apiPhp/users/" + UserId)
     var xhttp = new XMLHttpRequest();
-    
+
     xhttp.open("DELETE", "http://localhost/manager.one/apiPhp/users" + "/" + UserId, true);
     xhttp.send();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {loadUsers();}
+        if (this.readyState == 4 && this.status == 200) { loadUsers(); }
     }
 }
 
 // used to create a user
-function createUser()
-{
+function createUser() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "http://localhost/manager.one/apiPhp/users", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send(JSON.stringify({name: document.forms["createUserForm"]["name"].value, email:document.forms["createUserForm"]["email"].value}));
+    xhttp.send(JSON.stringify({ name: document.forms["createUserForm"]["name"].value, email: document.forms["createUserForm"]["email"].value }));
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {loadUsers();}
+        if (this.readyState == 4 && this.status == 200) { loadUsers(); }
     }
 }
 
 // Change the page to the user's task page
 function goToTask(UserId) {
-    document.location.href="http://localhost/manager.one/front/userTasks.html?user_id=" + UserId;
+    document.location.href = "http://localhost/manager.one/front/userTasks.html?user_id=" + UserId;
 }
