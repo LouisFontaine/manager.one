65924c2b8d6934fe98ea991d7fc783a63d67ca78
louisfontaine <louis.fontaine@efrei.net>
2019-12-22 21:40:25 +0100

    screen added

--- apiPhp/database.sql
+++ apiPhp/database.sql
@@ -36,10 +36,19 @@ CREATE TABLE `tasks` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO users (id, name, email) VALUES (null, 'louisfontaine',  'louis.fontaine@efrei.net');
+INSERT INTO users (id, name, email) VALUES (null, 'Louis',  'louis.fontaine@efrei.net');
+INSERT INTO `users` (`id`, `name`, `email`) VALUES (NULL, 'Marie', 'marielesang@gmail.com'), (NULL, 'Jérémie', 'jerembrebbo94@hotmail.fr');
 
 --
 -- Dumping data for table `tasks`
 --
 
 INSERT INTO tasks VALUES (null, 1, 'Talk to mama', 'talk to mama about how to wash my clothes', '2019-04-02 16:32:22', 'TODO');
+INSERT INTO tasks VALUES (null, 2, 'Talk to mama', 'talk to mama about how to wash my clothes', '2019-04-02 16:32:22', 'TODO');
+INSERT INTO tasks VALUES (null, 3, 'Talk to mama', 'talk to mama about how to wash my clothes', '2019-04-02 16:32:22', 'TODO');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '1', 'Work', 'Study for the english test', '2019-12-11', 'TODO');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '2', 'Work', 'Study for the english test', '2019-12-11', 'TODO');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '3', 'Work', 'Study for the english test', '2019-12-11', 'TODO');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '1', 'Clean', 'Clean the kitchen', '2019-12-14', 'DONE');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '2', 'Clean', 'Clean the kitchen', '2019-12-14', 'DONE');
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `creation_date`, `status`) VALUES (NULL, '3', 'Clean', 'Clean the kitchen', '2019-12-14', 'DONE');
